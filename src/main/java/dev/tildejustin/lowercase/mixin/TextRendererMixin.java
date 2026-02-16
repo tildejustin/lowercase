6ac282f9cd8f9b0e36ffa1bed973c9a1388231c1
tildejustin <tildejustin@gmail.com>
2026-02-16 17:32:36 -0500

    upgradle

--- src/main/java/dev/tildejustin/lowercase/mixin/TextRendererMixin.java
+++ src/main/java/dev/tildejustin/lowercase/mixin/TextRendererMixin.java
@@ -1,18 +1,20 @@
 package dev.tildejustin.lowercase.mixin;
 
 import net.minecraft.client.font.TextRenderer;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(TextRenderer.class)
 public class TextRendererMixin {
+    @Dynamic
     @ModifyVariable(
             method = {
-                    /* 1.19.4+ */ "drawInternal(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I",
+                    /* 1.19.4-1.21.6 */ "drawInternal(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I",
                     /* 1.19.3 */ "method_27529(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZIIZ)I",
                     /* 1.16.2-1.19.2 */ "method_27529(Ljava/lang/String;FFIZLnet/minecraft/class_1159;Lnet/minecraft/class_4597;ZIIZ)I"
             },
-            at = @At(value = "HEAD", ordinal = 0), argsOnly = true, allow = 1)
+            at = @At(value = "HEAD", ordinal = 0), argsOnly = true, allow = 1
+    )
     private String lowercase(String original) {
         return original.toLowerCase();
     }
